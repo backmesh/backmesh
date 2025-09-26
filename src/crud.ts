25afe864639ea1ac7d0391d1fa28fb898a934f1c
Luis Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2025-09-26 09:58:32 -0700

    generate events on proxy crud (#80)
    * generate events on proxy crud
    
    * rm unused dep
    
    * return proxy

--- src/crud.ts
+++ src/crud.ts
@@ -1,5 +1,6 @@
 import auth from './services/auth';
 import Subscription from './services/subscription';
+import posthog from './services/gateways/posthog';
 import { ProxyExchangeSummary } from './services/analytics';
 import { apiProxyCrud } from './services/proxy';
 import { stripeIntegrationCrud } from './services/stripe';
@@ -126,9 +127,11 @@ async function handleProxyRequest(
 				return new Response('No body in request', { status: 400 });
 			}
 			const requestUrl = new URL(request.url);
-			return handleRequest(async () =>
-				apiProxyCrud.create(env, requestUrl.origin, backmeshUid, await request.json()),
-			);
+			return handleRequest(async () => {
+				const proxy = await apiProxyCrud.create(env, requestUrl.origin, backmeshUid, await request.json());
+				await posthog.captureNewProxy(backmeshUid, proxy.id!, env);
+				return proxy;
+			});
 
 		case 'PUT':
 			if (proxyId === undefined) {
@@ -137,9 +140,11 @@ async function handleProxyRequest(
 			if (!request.body) {
 				return new Response('No body in request', { status: 400 });
 			}
-			return handleRequest(async () =>
-				apiProxyCrud.edit(env, backmeshUid, proxyId!, await request.json()),
-			);
+			return handleRequest(async () => {
+				const proxy = await apiProxyCrud.edit(env, backmeshUid, proxyId!, await request.json());
+				await posthog.captureEditProxy(backmeshUid, proxyId!, env);
+				return proxy;
+			});
 
 		case 'GET':
 			return handleRequest(async () => {
@@ -158,7 +163,11 @@ async function handleProxyRequest(
 			if (proxyId === undefined) {
 				return new Response('Invalid pathname', { status: 400 });
 			}
-			return handleRequest(async () => apiProxyCrud.delete(env, backmeshUid, proxyId!));
+			return handleRequest(async () => {
+				const proxy = await apiProxyCrud.delete(env, backmeshUid, proxyId!);
+				await posthog.captureDeleteProxy(backmeshUid, proxyId!, env);
+				return proxy;
+			});
 
 		default:
 			return new Response('Not Found', { status: 404 });
--- src/services/gateways/posthog.ts
+++ src/services/gateways/posthog.ts
@@ -16,6 +16,42 @@ async function captureEvent(name: string, properties: any) {
 }
 
 export default {
+	async captureNewProxy(
+		backmeshUid: string,
+		proxyId: string,
+		env: any,
+	) {
+		if (env.TEST_USER_PASS !== undefined) return;
+		await captureEvent('new_proxy', {
+			distinct_id: backmeshUid,
+			proxy_id: proxyId,
+		});
+	},
+
+	async captureDeleteProxy(
+		backmeshUid: string,
+		proxyId: string,
+		env: any,
+	) {
+		if (env.TEST_USER_PASS !== undefined) return;
+		await captureEvent('delete_proxy', {
+			distinct_id: backmeshUid,
+			proxy_id: proxyId,
+		});
+	},
+
+	async captureEditProxy(
+		backmeshUid: string,
+		proxyId: string,
+		env: any,
+	) {
+		if (env.TEST_USER_PASS !== undefined) return;
+		await captureEvent('edit_proxy', {
+			distinct_id: backmeshUid,
+			proxy_id: proxyId,
+		});
+	},
+
 	async captureProxyReq(
 		req: ProxyRequest | InvalidProxyRequest,
 		res: ProxyResponse,
